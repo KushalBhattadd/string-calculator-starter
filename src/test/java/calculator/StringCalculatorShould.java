748762ac4dad64685e54e91a257dd07cee4454e6
Kushal_Bhattad <kabhattad@gmail.com>
2021-10-16 16:41:53 +0530

    test case 1 empty_string_should_return_zero

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,19 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
+   StringCalculator c;
+   @BeforeEach
+    void begin(){
+       c = new StringCalculator();
+   }
     @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
-
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    void empty_string_should_return_zero(){
+        assertEquals(0,c.add(" "));
     }
 }
